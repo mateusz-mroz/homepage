28ff062595552625b8b20a162ee6fc43f7f99aaa
Mateusz Mróz <mroz92mateusz@gmail.com>
2022-09-11 20:10:15 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Mateusz';
+const age = 30;
+
+alert('kolejność działa od góry do dołu');
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
